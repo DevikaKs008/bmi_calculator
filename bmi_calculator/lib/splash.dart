89400919dbbe9695288e52fe7e9931f7e76abfe4
DevikaKs008 <devugopu008@gmail.com>
2026-02-05 15:07:44 +0530

    first commit

---
+++ bmi_calculator/lib/splash.dart
@@ -0,0 +1,73 @@
+import 'package:bmi_calculator/bmi.dart';
+import 'package:flutter/material.dart';
+
+class Splash extends StatelessWidget {
+  const Splash({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        height: double.infinity,
+        width: double.infinity,
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            colors: [
+              const Color.fromARGB(255, 190, 167, 139),
+              const Color.fromARGB(255, 215, 136, 18),
+            ],
+            begin: Alignment.topLeft,
+            end: Alignment.bottomLeft,
+          ),
+          border: Border.all(color: Colors.black),
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              "BMI \n CALCULATOR",
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontFamily: "poppins",
+                fontSize: 40,
+                fontWeight: FontWeight.bold,
+                color: const Color.fromARGB(255, 18, 17, 17),
+              ),
+            ),
+
+            Text(
+              "Calculate Your Body \n Mass Index easily !",
+              style: TextStyle(
+                fontFamily: "italic",
+                fontSize: 28,
+                color: const Color.fromARGB(255, 18, 17, 17),
+              ),
+            ),
+            SizedBox(height: 150),
+            SizedBox(
+              height: 100,
+              width: 180,
+              child: ElevatedButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => Bmi()),
+                  );
+                },
+
+                child: Text("GET STARTED"),
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: const Color.fromARGB(255, 22, 7, 2),
+                  foregroundColor: Colors.white,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
